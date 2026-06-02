d6da2c400b50ac80b50ec3c08d04c95a253fe8f3
Hakata <literallyme098@gmail.com>
2026-06-02 21:32:53 +0545

    final code , decoding

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-with open("story.txt","r") as f:
-    story = f.read()
 
+from google import genai
+import  os
+key = os.environ.get('api_key')
+client = genai.Client(api_key = key)
+
+response = client.models.generate_content(
+    model="gemini-3.5-flash", contents="generate stiory story for mad libs , and use <angle bracket > for placeholders , for certain words like <adjective> , <food> etc which are to be filled by the player/user the place holders should be <angle brackets > only , not () , not {} or anything  and in those  angle bracketers there is only words , no spaces in front or at the back of the angle brackets for the story , donot generate too much placeholder words neither too less , it should be of perfect length , the length suitable for a game to feel fun but not tooo long like typing a message  Dont make the story too long or too short , just the perfect length / medium Do not include anyhthin except the story , not even  Hey , here is the story  And generate different kind of stories , take multiple genres from the story books and all , make multiple settings and multiple and different characters each time"
+)
+
+story = response.text
 
 
 target_start = "<"
@@ -21,9 +29,9 @@ answers = {}
 
 for word in words:
     answer = input("Enter a word for " + word + ": ")
-    answers[word] = answer 
+    answers[word] = answer  
 
 for word in words: 
     story = story.replace(word,answers[word])
 
-print(story)
+print(story)    
